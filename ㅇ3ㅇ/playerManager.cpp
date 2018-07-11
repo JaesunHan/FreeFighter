7e0b8fad1940b5035d179a2574c2133a2a9865a4
KingBUG <toobangi@naver.com>
2018-07-11 18:41:37 +0900

    매니져져져

--- ㅇ3ㅇ/playerManager.cpp
+++ ㅇ3ㅇ/playerManager.cpp
@@ -33,7 +33,7 @@ void playerManager::Update()
 {
 	if (KEYMANAGER->isOnceKeyDown('M'))
 	{
-		setGigas(D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(0, 0, 0));
+		setKnight();
 	}
 
 	//if (KEYMANAGER->isOnceKeyDown('N'))
@@ -63,16 +63,19 @@ void playerManager::Render()
 	}
 }
 
-void playerManager::setLucius(D3DXVECTOR3 scale, D3DXVECTOR3 position)
+void playerManager::setLucius()
 {
 	player* _lucius = new lucius;
 	_lucius->Init(PLAYER_1P, _T(".\\xFile\\lucius"), _T("lucius.X"));
+	_lucius->createContoller(&_cm, _material);
+	_lucius->SetSRT(D3DXVECTOR3(0.005f, 0.005f, 0.005f), D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(0, 0, 0));
+
 
 	_vPlayer.push_back(_lucius);
 
 }
 
-void playerManager::setGigas(D3DXVECTOR3 scale, D3DXVECTOR3 position)
+void playerManager::setGigas()
 {
 	player* _gigas = new gigas;
 	_gigas->Init(PLAYER_1P, _T(".\\xFile\\gigas"), _T("gigas.X"));
@@ -83,19 +86,25 @@ void playerManager::setGigas(D3DXVECTOR3 scale, D3DXVECTOR3 position)
 
 }
 
-void playerManager::setAranzebia(D3DXVECTOR3 scale, D3DXVECTOR3 position)
+void playerManager::setAranzebia()
 {
 	aranzebia* _aranzebia = new aranzebia;
 	_aranzebia->Init(PLAYER_1P, _T(".\\xFile\\aranzebia"), _T("aranzebia.X"));
+	_aranzebia->createContoller(&_cm, _material);
+	_aranzebia->SetSRT(D3DXVECTOR3(0.005f, 0.005f, 0.005f), D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(0, 0, 0));
+
 
 	_vPlayer.push_back(_aranzebia);
 
 }
 
-void playerManager::setKnight(D3DXVECTOR3 scale, D3DXVECTOR3 position)
+void playerManager::setKnight()
 {
 	knight* _knight = new knight;
 	_knight->Init(PLAYER_1P, _T(".\\xFile\\knight"), _T("knight.X"));
+	_knight->createContoller(&_cm, _material);
+	_knight->SetSRT(D3DXVECTOR3(0.005f, 0.005f, 0.005f), D3DXVECTOR3(0, 0, 0), D3DXVECTOR3(0, 0, 0));
+
 
 
 	_vPlayer.push_back(_knight);
